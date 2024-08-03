b7bc22ff9ac1116a3ec65793176578ce45844b1a
Andres <andrespodadera87@gmail.com>
2024-08-03 13:56:27 +0200

    cap 2 finished

--- firstConsoleApp/Program.cs
+++ firstConsoleApp/Program.cs
@@ -1,2 +1,84 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+OperatorExamples();
+
+void OperatorExamples()
+{
+    // This statement declares a variable and sets it to 3
+    int width = 3;
+
+    // The ++ operator increments a variable(adds 1 to it)
+    width++;
+
+    // Declare two  more int variables to hold numbers and
+    // use the + and * operators to add and multiply values
+    int height = 2 + 4;
+    int area = width * height;
+    Console.WriteLine(area);
+
+    while (area < 50)
+    {
+        height++;
+        area = width * height;
+    }
+
+    do
+    {
+        width--;
+        area = width * height;
+    } while (area > 25);
+
+    // The next two statements declare string variables
+    // and use + to concatenate them (join them together)
+    string result = "The area";
+    result = result + " is " + area;
+    Console.WriteLine(result);
+
+    // A Boolean variable is either true or false
+    bool truthValue = true;
+    Console.WriteLine(truthValue);
+}
+
+TryAnIf();
+TrySomeLoops();
+TryAnIfElse();
+
+static void TryAnIf()
+{
+    int someValue = 4;
+    string name = "Bobbo Jr.";
+    if ((someValue == 3) && (name == "Joe"))
+    {
+        Console.WriteLine("x is 3 and the name is Joe");
+    }
+    Console.WriteLine("this line runs no matter what");
+}
+
+static void TryAnIfElse()
+{
+    int x = 5;
+    if (x == 10)
+    {
+        Console.WriteLine("x must be 10");
+    }
+    else
+    {
+        Console.WriteLine("x isn’t 10");
+    }
+}
+
+static void TrySomeLoops()
+{
+    int count = 0;
+
+    while (count < 10)
+    {
+        count = count + 1;
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        count = count - 1;
+    }
+
+    Console.WriteLine("The answer is " + count);
+}
--- firstConsoleApp/obj/Debug/net8.0/firstConsoleApp.AssemblyInfo.cs
+++ firstConsoleApp/obj/Debug/net8.0/firstConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("firstConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20186e5034ce6b5043edf012e6ae12c77f4ffda7")]
 [assembly: System.Reflection.AssemblyProductAttribute("firstConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("firstConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
